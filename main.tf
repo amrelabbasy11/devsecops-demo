2b5d3fa8e4f2bc8e1053d4838ef5ef128305bd9d
Amr Elabassy <180682735+amrelabbasy11@users.noreply.github.com>
2025-07-02 10:28:59 +0300

    Update main.tf with IAM and S3

--- main.tf
+++ main.tf
@@ -1,3 +1,34 @@
+provider "aws" {
+  region = "eu-central-1"
+}
+
+# Insecure S3 bucket (for demo purposes - tfsec will flag this)
+resource "aws_s3_bucket" "amr_bucket" {
+  bucket = "amr-devsecops-demo-bucket"
+  acl    = "public-read" # tfsec will detect this as insecure
+
+  tags = {
+    Owner = "Amr"
+    Env   = "Dev"
+  }
+}
+
+# IAM Policy for read-only access (best practice enforcement)
+resource "aws_iam_policy" "readonly" {
+  name   = "readonly-policy"
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Effect = "Allow",
+        Action = [
+          "s3:GetObject"
+        ],
+        Resource = "*"
+      }
+    ]
+  })
+}
 # main.tf
 provider "aws" {
   region = "eu-central-1"
