570a604a9ed920ade7cd7c5245c3c702d3ffc7f8
Amr Elabassy <180682735+amrelabbasy11@users.noreply.github.com>
2025-07-02 16:40:00 +0300

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,22 @@
+# Provider for my application resources
 provider "aws" {
+  alias  = "app"
   region = "eu-central-1"
 }
 
-# Insecure S3 bucket (for demo purposes - tfsec will flag this)
+# Provider for AWS Organizations (must be us-east-1)
+provider "aws" {
+  alias  = "org"
+  region = "us-east-1"
+}
+
+#########################
+# S3 bucket (demo, insecure ACL)
+#########################
 resource "aws_s3_bucket" "amr_bucket" {
-  bucket = "amr-devsecops-demo-bucket"
-  acl    = "public-read" # tfsec will detect this as insecure
+  provider = aws.app
+  bucket   = "amr-devsecops-demo-bucket"
+  acl      = "public-read"  # This is insecure, will be blocked by SCP
 
   tags = {
     Owner = "Amr"
@@ -13,9 +24,12 @@ resource "aws_s3_bucket" "amr_bucket" {
   }
 }
 
-# IAM Policy for read-only access (best practice enforcement)
+#########################
+# IAM Policy for read-only S3 access
+#########################
 resource "aws_iam_policy" "readonly" {
-  name   = "readonly-policy"
+  provider = aws.app
+  name     = "readonly-policy"
   policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
@@ -29,18 +43,42 @@ resource "aws_iam_policy" "readonly" {
     ]
   })
 }
-# main.tf
-provider "aws" {
-  region = "eu-central-1"
+
+#########################
+# Organization SCP that denies public-read ACL on S3 buckets
+#########################
+resource "aws_organizations_policy" "deny_public_s3" {
+  provider    = aws.org
+  name        = "DenyPublicS3Access"
+  description = "Deny creating or modifying S3 buckets with public-read ACL"
+  type        = "SERVICE_CONTROL_POLICY"
+  content     = <<POLICY
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Deny",
+      "Action": [
+        "s3:PutBucketAcl",
+        "s3:PutObjectAcl"
+      ],
+      "Resource": "*",
+      "Condition": {
+        "StringEquals": {
+          "s3:x-amz-acl": "public-read"
+        }
+      }
+    }
+  ]
+}
+POLICY
 }
 
-resource "aws_s3_bucket" "amr_bucket" {
-  bucket = "amr-devsecops-demo-bucket"
-  acl    = "public-read"  # tfsec will warn about this
-
-  tags = {
-    Owner = "Amr"
-    Env   = "Dev"
-  }
+#########################
+# Attach the SCP to your AWS Organization root
+#########################
+resource "aws_organizations_policy_attachment" "deny_public_s3_attach" {
+  provider  = aws.org
+  policy_id = aws_organizations_policy.deny_public_s3.id
+  target_id = "r-f7ni" 
 }
-
